2e3422a00cdd470562dc5f160a4895d61d3f2907
laiiihz <laiiihz@outlook.com>
2020-06-03 16:20:55 +0800

    [header] add dark [header] add backgroundColor

--- src/components/header/AHeader.vue
+++ src/components/header/AHeader.vue
@@ -10,12 +10,23 @@
         'is-under-line': underLine,
       },
     ]"
-    :style="height ? 'height:' + height + 'px' : ''"
+    :style="{
+      height: height ? height + 'px' : '',
+      backgroundColor: backgroundColor
+        ? backgroundColor
+        : dark
+        ? 'rgba(0,0,0,.7)'
+        : 'rgba(255,255,255,.7)',
+    }"
   >
     <div class="a-header__leading">
       <slot name="leading"></slot>
     </div>
-    <span class="a-header__title"><slot></slot></span>
+    <span
+      class="a-header__title"
+      :style="{ color: dark ? 'rgba(255,255,255,.9)' : 'rgba(0,0,0,.9)' }"
+      ><slot></slot
+    ></span>
     <div class="a-header__menus">
       <slot name="menu"></slot>
     </div>
@@ -32,6 +43,8 @@ export default {
     height: Number,
     flat: Boolean,
     underLine: Boolean,
+    dark: Boolean,
+    backgroundColor: String,
   },
 };
 </script>
