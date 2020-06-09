2812964e00b447c8dc7a4998f1cd6ff6bf651870
laiiihz <laiiihz@outlook.com>
2020-06-09 14:13:46 +0800

    [template] fix font page

--- template/src/page/FontPage.vue
+++ template/src/page/FontPage.vue
@@ -3,7 +3,9 @@
     <a-h :dark="dark">字体</a-h>
     <a-h h="2" :dark="dark">字体下载安装</a-h>
     <a-h h="3" :dark="dark"
-      >推荐使用<a-link :dark="dark" type="primary"
+      >推荐使用<a-link
+        :dark="dark"
+        type="primary"
         href="https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKsc-hinted.zip"
         >Noto Sans<i class="material-icons">link</i></a-link
       >
