17f476e41e82abf87cbc4ecde5dada97b8aa6809
laiiihz <laiiihz@outlook.com>
2020-06-11 21:04:54 +0800

    [drawer] update drawer component

--- src/components/drawer/ADrawer.vue
+++ src/components/drawer/ADrawer.vue
@@ -1,13 +1,98 @@
 <template>
-    <div :class="[]"></div>
+  <transition name="fade">
+    <div
+      @click.self="handleClick"
+      :class="['a-drawer-mask']"
+      :style="blur ? `backdrop-filter: blur(${blurValue})` : ''"
+      v-show="visible"
+    >
+      <transition :name="`${direction}-animation`">
+        <div
+          v-show="visible"
+          :class="['a-drawer', 'a-drawer--' + direction, { 'is-dark': dark }]"
+          :style="[this.widthStyle, this.heightStyle]"
+        ></div>
+      </transition>
+    </div>
+  </transition>
 </template>
 
 <script>
-    export default {
-        name: "ADrawer"
-    }
+export default {
+  name: "ADrawer",
+  props: {
+    dark: Boolean,
+    blur: {
+      type: [Boolean, Number, String],
+    },
+    visible: Boolean,
+    direction: {
+      type: String,
+      default: "left",
+      validator: (val) => ["left", "right", "top", "bottom"].includes(val),
+    },
+    width: {
+      type: [Number, String],
+    },
+    height: {
+      type: [Number, String],
+    },
+  },
+  computed: {
+    blurValue() {
+      if (typeof this.blur == "boolean") {
+        return "5px";
+      } else if (typeof this.blur == "number") {
+        return `${this.blur}px`;
+      } else {
+        if (this.blur.indexOf("px") === -1) {
+          return `${this.blur}px`;
+        }
+        return this.blur;
+      }
+    },
+    widthStyle() {
+      switch (typeof this.width) {
+        case "number":
+          return { width: `${this.width}px` };
+        case "string":
+          if (this.width.indexOf("px") === -1)
+            return { width: `${this.width}px` };
+          else return { width: this.width };
+        case "undefined":
+        default:
+          return {};
+      }
+    },
+    heightStyle() {
+      switch (typeof this.height) {
+        case "string":
+          if (this.height.indexOf("px") === -1)
+            return { height: `${this.height}px` };
+          else return { height: this.height };
+        case "number":
+          return { height: `${this.height}px` };
+        case "undefined":
+        default:
+          return {};
+      }
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$emit("update:visible", false);
+    },
+  },
+  watch: {},
+};
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+@import "../style/animations";
+@include animation_enter("fade", 0.3s) {
+  opacity: 0;
+}
+@include animation_leave("fade", 0.3s) {
+  opacity: 0;
+}
 </style>
