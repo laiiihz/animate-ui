24b1764e55280928f24b5a7aeae93451c8be7d03
laiiihz <laiiihz@outlook.com>
2020-06-19 22:00:31 +0800

    [template]:(break change) update code preview component api

--- template/src/components/CodePreview.vue
+++ template/src/components/CodePreview.vue
@@ -8,11 +8,12 @@
     <a-button
       v-for="item in codeTypes"
       :key="item"
-      text
       :dark="dark"
+      :type="current === item ? 'primary' : null"
       @click="clickBlock(item)"
-      >{{ item }}</a-button
     >
+      {{ item }}
+    </a-button>
     <transition name="slide-x" mode="out-in">
       <code-block
         ref="blockRef"
@@ -33,13 +34,13 @@ export default {
   methods: {
     clickBlock(item) {
       this.current = item;
-      this.value = this[item];
+      this.value = this.valueObject[item];
       this.updateHeight();
     },
     updateHeight() {
       this.timer = setTimeout(() => {
         this.$refs.previewRef.style.height =
-          this.$refs.blockRef.$el.firstElementChild.offsetHeight + 45+ "px";
+          this.$refs.blockRef.$el.firstElementChild.offsetHeight + 45 + "px";
       }, 500);
     },
     mouseLeave() {
@@ -48,21 +49,15 @@ export default {
     },
   },
   props: {
-    html: String,
-    js: String,
-    css: String,
     dark: Boolean,
-    default: {
-      type: String,
-      default: "html",
-    },
+    valueObject: Object,
   },
   mounted() {
-    this.current = this.default;
-    this.value = this[this.current];
-    this.html ? this.codeTypes.push("html") : null;
-    this.js ? this.codeTypes.push("js") : null;
-    this.css ? this.codeTypes.push("css") : null;
+    this.valueObject["html"] ? this.codeTypes.push("html") : null;
+    this.valueObject["js"] ? this.codeTypes.push("js") : null;
+    this.valueObject["css"] ? this.codeTypes.push("css") : null;
+    this.current = this.codeTypes[0];
+    this.value = this.valueObject[this.codeTypes[0]];
     this.$refs.previewRef.style.height = "45px";
   },
   data: () => ({
@@ -83,6 +78,6 @@ export default {
   transition: 0.3s;
 }
 .is-out-dark {
-  border: 1px solid rgba(255, 255, 255, 0.4);
+  border: 2px solid rgba(255, 255, 255, 0.4);
 }
 </style>
